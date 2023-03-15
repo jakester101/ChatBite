68682c515e487a446ed3cc81d308a6d0671743a9
Andres-Zetina <zetinaandres700@gmail.com>
2023-03-15 11:50:01 -0400

    deploying

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -35,11 +35,11 @@ module.exports = {
             return res.status(500).json({message: 'server error'})
         }
       },
+    //logout logic 
       async logout(req, res) {
-        const {userId} = req.body;
-
+        const {_id} = req.body;
         try{
-            const user = await User.findById(userId);
+            const user = await User.findById(_id);
 
             if(!user) {
                 return res.status(404).json({message: "user not found"})

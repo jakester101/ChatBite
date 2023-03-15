c00ba98b02c5e9b6448a97489ffda35a87eecd13
Junho Kim <kjh01222@gmail.com>
2023-03-15 16:55:35 -0400

    added tailwind theme

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,6 +4,22 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    minHeight: {
+      '50vh': '50vh',
+      '40vh': '40vh',
+      '30vh': '30vh'
+    },
+    minWidth: {
+      '50vh': '50vh',
+      '70vh': '70vh'
+    },
+    maxWidth: {
+      '50vh': '50vh',
+      '20vh': '20vh'
+    },
+    maxHeight: {
+      '40vh': '40vh',
+    },
     extend: {},
   },
   plugins: [],

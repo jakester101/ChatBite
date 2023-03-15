af47a1f2f6dea72c1277e4d0bb8ce59b58921714
Junho Kim <kjh01222@gmail.com>
2023-03-15 16:44:07 -0400

    SingleRecipe.js and RecipeCard.js

--- client/src/pages/RecipeCard.js
+++ client/src/pages/RecipeCard.js
@@ -14,31 +14,37 @@ const RecipeCard = ({ recipe, image, onSave }) => {
   ));
 
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg">
-      <img className="w-full" src={image.data[0].url} alt={recipe.name} />
-      <div className="px-6 py-4">
-        <div className="font-bold text-xl mb-2">{recipe.name}</div>
-        <p className="text-gray-700 text-base">{recipe.description}</p>
-        <br />
-        <p className="text-gray-700 text-base">
-          <strong>Instructions:</strong>
+    <div className='flex justify-center items-center h-screen flex-col gap-y-10'>
+      <div className="max-w-50vh min-h-40vh rounded overflow-hidden shadow-2xl">
+        <div className='flex justify-center min-w-max py-5 bg-purple-700'>
+          <img className="max-w-20vh" src={image.data[0].url} alt={recipe.name} />
+        </div>
+        <div className="px-6 py-4">
+          <div className="font-bold text-xl mb-2">{recipe.name}</div>
+          <blockquote className="text-gray-700 text-base"> <strong>{recipe.description}</strong>
+          <br/>
+          </blockquote>
           <br />
-          {instructions}
-        </p>
-        <p className="text-gray-700 text-base">
-          <strong>Prep Time:</strong> {recipe.prepTime}
-        </p>
-        <p className="text-gray-700 text-base font-bold">
-          <strong>Calorie Count:</strong> {recipe.calorieCount}
-        </p>
-      </div>
-      <div className="px-6 pt-4 pb-2">
-      <button onClick = {onSave} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-        Save
-      </button>
+          <blockquote className="text-gray-700 text-base">
+            <strong>Instructions:</strong>
+            <br />
+            {instructions}
+          </blockquote>
+          <p className="text-gray-700 text-base">
+            <strong>Prep Time:</strong> {recipe.prepTime}
+          </p>
+          <p className="text-gray-700 text-base font-bold">
+            <strong>Calorie Count:</strong> {recipe.calorieCount}
+          </p>
+        </div>
+        <div className="px-6 pt-4 pb-2">
+        <button onClick = {onSave} className="bg-purple-700 hover:bg-purple-300 text-white font-bold py-2 px-4 rounded">
+          Save
+        </button>
+        </div>
       </div>
     </div>
-  );
+    );
 };
 
 export default RecipeCard;

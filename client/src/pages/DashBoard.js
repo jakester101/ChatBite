2e3ab8a640d682e134f9300327f2073dab9601a5
Andres-Zetina <zetinaandres700@gmail.com>
2023-03-15 18:15:32 -0400

    got rid of extra searchbar that did nothing

--- client/src/pages/DashBoard.js
+++ client/src/pages/DashBoard.js
@@ -25,7 +25,7 @@ const DashBoard = () => {
     }, []);
 
   
-    console.log(rList);
+    // console.log(rList);
     let list = rList.map((recipe) => (<button className="w-full shadow-xl px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600"key={recipe._id} onClick = {console.log(`${recipe.name} selected`)}>{recipe.name}</button>));
 
     const handleFetchData = async (params) => {
@@ -63,30 +63,30 @@ const DashBoard = () => {
             <div>place holder text</div>
         </div>
 
-        <div className="w-3/4 p-8 h-screen text-2xl">
-            <h2>Hungry but dont know what to make? </h2>
+        {/* <div className="w-3/4 p-8 h-screen text-2xl"> */}
+            {/* <h2>Hungry but dont know what to make? </h2>
             Type in what ingredients you have and we will make a recipe for you!
             <br />
             Please type them in one at a time separated by a space and a comma.
             <br />
-            Then click the "Make New Recipe".
+            Then click the "Make New Recipe". */}
             <div className="w-3/4 p-8 h-screen text-3xl">
                 {/* <h2>Hungry but dont know what to make?<br />Type in what ingredients you have and we will make a recipe for you!</h2> */}
-                <input
+                {/* <input
                     type="text"
                     className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
                     placeholder="Search"
-                    />
-                    <div className="mt-6 ">
-                        <button className="w-full shadow-xl px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600">
+                    /> */}
+                    {/* <div className="mt-6 "> */}
+                        {/* <button className="w-full shadow-xl px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600">
                             Search
-                        </button>
-                    </div>
-                    <br></br>
+                        </button> */}
+                    {/* </div> */}
+                    {/* <br></br> */}
                     <ul>
                         {list}
                     </ul>
-             </div>
+             {/* </div> */}
              <div className="w-3/4 p-8 h-screen text-3xl">
              <h2>Hungry but dont know what to make?<br />Type in what ingredients you have and we will make a recipe for you!</h2>
              <input

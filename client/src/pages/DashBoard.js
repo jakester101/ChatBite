19a105e15e483c57dc337b0072ec9d01d93dd91a
Andres-Zetina <zetinaandres700@gmail.com>
2023-03-15 21:29:56 -0400

    added fonts and some minor styling changes

--- client/src/pages/DashBoard.js
+++ client/src/pages/DashBoard.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from "react";
 import { fetchData } from "../fetch";
 import { recipeList } from "../fetch";
 import RecipeCard from './RecipeCard'; // Import RecipeCard component
-
+import Banner from "./Banner";
 import { useMutation } from '@apollo/client';
 import { ADD_RECIPE } from '../utils/mutations';
 
@@ -70,8 +70,8 @@ const DashBoard = () => {
     };
     
     return (
-      <div className="w-full flex justify-center h-screen">
-        <div className="w-1/4 p-8 border-solid border-r-4 h-screen">
+      <div style={{fontFamily:'Space Mono'}} className="w-full flex justify-center h-screen bg-white">
+        <div className="w-1/4 p-8 border-solid border-r-4 h-screen text-center">
             Previous Recipes
             <input
                 type="text"
@@ -89,31 +89,12 @@ const DashBoard = () => {
             <div>place holder text</div>
         </div>
 
-        {/* <div className="w-3/4 p-8 h-screen text-2xl"> */}
-            {/* <h2>Hungry but dont know what to make? </h2>
-            Type in what ingredients you have and we will make a recipe for you!
-            <br />
-            Please type them in one at a time separated by a space and a comma.
-            <br />
-            Then click the "Make New Recipe". */}
             <div className="w-3/4 p-8 h-screen text-3xl">
-                {/* <h2>Hungry but dont know what to make?<br />Type in what ingredients you have and we will make a recipe for you!</h2> */}
-                {/* <input
-                    type="text"
-                    className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
-                    placeholder="Search"
-                    /> */}
-                    {/* <div className="mt-6 "> */}
-                        {/* <button className="w-full shadow-xl px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600">
-                            Search
-                        </button> */}
-                    {/* </div> */}
-                    {/* <br></br> */}
                     <ul>
                         {list}
                     </ul>
-             {/* </div> */}
-             <div className="w-3/4 p-8 h-screen text-3xl">
+             
+             <div className="w-3/4 p-8 h-screen text-3xl text-center">
              <h2>Hungry but dont know what to make?<br />Type in what ingredients you have and we will make a recipe for you!</h2>
              <input
                 type="text"
@@ -132,8 +113,10 @@ const DashBoard = () => {
                     >
                         Make New Recipe
                     </button>
+                    <br></br>
                 </div>
                 <div className="flex justify-center items-center h-full">
+                    
                     <RecipeCard recipe={recipe} image={image} onSave={saveRecipe} />
                 </div>
             </div>
--- client/src/pages/Header.js
+++ client/src/pages/Header.js
@@ -6,7 +6,7 @@ import React from "react";
 const Header = () => {
    
     return (
-        <div>
+        <div style={{fontFamily:'Orbitron'}}>
             <header className="w-full shadow-xl p-4 mb-2 bg-purple-800">
                 <h2 className="text-center text-6xl text-white">ChatBite</h2>
             </header>
